7900b3020c7a667a9631798ac5ee1468bdbe9396
MignaTech <migna0519@gmail.com>
2023-05-22 14:08:51 -0600

    Clase de Conexión a la BD de Postgresql

---
+++ proyectoSHIELD/src/main/java/datos/Conexion.java
@@ -0,0 +1,46 @@
+package datos;
+import java.sql.*;
+
+public class Conexion {
+    private static String user = "postgres";
+    private static String pswd = "123456";
+    private static String bd = "pro_Shield";
+    private static String server = "jdbc:postgresql://localhost:5432/" + bd;
+    private static String driver = "org.postgresql.Driver";
+    public static Connection getConnection() {
+        try {
+            Class.forName(driver);
+            return DriverManager.getConnection(server, user, pswd);
+        } catch (SQLException ex) {
+            System.out.println("Error al intentar conectarse a la BD " + bd);
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
+        return null;
+    }
+
+    public static void close(ResultSet result){
+        try {
+            result.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    public static void close(PreparedStatement state){
+        try {
+            state.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    public static void close(Connection con){
+        try {
+            con.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+}
